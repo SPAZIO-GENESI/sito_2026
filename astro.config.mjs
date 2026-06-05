8f64abf5ec8bf2cb98cdd21094bfb9065107988c
maxAlone <mascam64@gmail.com>
2026-06-05 09:37:46 +0200

    rimosso base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://spazio-genesi.github.io',
-  base: '/sito_2026',
+  
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
